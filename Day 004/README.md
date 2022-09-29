df664ff7b4c325b34fe90f7c5f629511e0086cbb
Jahswill Ovedhe <jahswilling@gmail.com>
2022-09-29 23:48:24 +0100

    Day 4(Serverless with lambda sns dynamodb and api gate way )

---
+++ Day 004/README.md
@@ -0,0 +1,17 @@
+![banner image](./img/heading.png)
+
+Day 004
+===
+
+I kept working on the serverless application on day 4. The work was more or less in hand on a lambda function (mail_lambda.py file) that publishes the SNS topic we had previously built and inserts the data into the dynamo database we had created yesterday. The lambda was completed today remaining the next/last steps which involve connecting it to API gate-way and integrating on the index.html page.
+
+##Overview of the lambda function test:<br/>
+![Network settings](./img/test.png)
+![Network settings](./img/email.png)
+![Network settings](./img/db_entries.png)
+
+
+
+## Outstanding Challenges
+Cloud front creation blocked by aws
+![Network settings](./img/cloundfront.png)
---
+++ Day 004/mail_lambda.py
@@ -0,0 +1,46 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+import datetime
+import os
+
+def send_sns(message, subject):
+    client = boto3.client("sns")
+    topic_arn = os.environ["Sns_topic"]
+    response = client.publish(
+        TopicArn=topic_arn, Message=message, Subject=subject)
+    return response
+    
+def save_to_db(data):
+    
+    client_dynamo=boto3.resource('dynamodb')
+    
+    table_name = os.environ["Table_name"]
+    table=client_dynamo.Table(table_name)
+    
+    response=table.put_item(Item=data)
+    
+    return response
+
+def lambda_handler(event, context):
+    
+    data = json.loads(event['body'])
+
+    create_at = datetime.datetime.now()
+    
+    data['create_at'] = create_at.strftime("%m/%d/%Y, %H:%M:%S")
+    
+    try:
+        save_to_db(data)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    
+    try:
+        send_sns(json.dumps(data),"testing")
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Email successfully sent!')
+    }
